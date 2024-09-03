43ee535bb11bc521c9fc274856f3046f22ce95fd
pitzee <pitzee421@gmail.com>
2024-09-03 07:58:59 -0400

    register updated

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,15 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import RegisterPanel from './components/Register/Register';
+import LogoutPanel from './components/Logout/Logout';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/logout" element={<LogoutPanel />} />
     </Routes>
   );
 }
