7384217144248cb46ab6ee0ce4e748abba369d78
pitzee <pitzee421@gmail.com>
2024-09-06 03:16:16 -0400

    dealer ship

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import RegisterPanel from './components/Register/Register';
 import LogoutPanel from './components/Logout/Logout';
+import Dealers from './components/Dealers/Dealers';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/logout" element={<LogoutPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
